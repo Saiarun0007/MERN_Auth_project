f741383ee2e28ee87ac2eb16ce85faf6a74b8f59
unknown <lifesaver0510@gmail.com>
2025-09-10 11:21:36 +0530

    insital git intalled tailwind and react

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold bg-amber-800 text-amber-50 w-full h-fit justify-center flex align-middle align-items-center rounded-s-lg">Welcome to the App</h1>
+    </div>
+  )
+}
